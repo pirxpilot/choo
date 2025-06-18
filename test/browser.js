b8a75bfcb55fa890e34a63f5c76652670b1e38a6
Damian Krzeminski <pirxpilot@furkot.com>
2025-06-17 20:31:04 -0600

    replace `tape` with `node:test`

--- test/browser.js
+++ test/browser.js
@@ -4,13 +4,13 @@ be(['window', 'document'], {
   pretendToBeVisual: true
 });
 
-const tape = require('tape');
+const test = require('node:test');
 
 const choo = require('..');
 
 globalThis.DEBUG = true;
 
-tape('should mount in the DOM', t => {
+test('should mount in the DOM', (t, done) => {
   t.plan(1);
   const app = choo();
   const container = init('/', 'p');
@@ -18,7 +18,8 @@ tape('should mount in the DOM', t => {
     const strong = '<strong>Hello filthy planet</strong>';
     window.requestAnimationFrame(() => {
       const exp = '<p><strong>Hello filthy planet</strong></p>';
-      t.equal(container.outerHTML, exp, 'result was OK');
+      t.assert.equal(container.outerHTML, exp, 'result was OK');
+      done();
     });
     const p = document.createElement('p');
     p.innerHTML = strong;
@@ -27,69 +28,70 @@ tape('should mount in the DOM', t => {
   app.mount(container);
 });
 
-tape('should expose a public API', t => {
+test('should expose a public API', t => {
   const app = choo();
 
-  t.equal(typeof app.route, 'function', 'app.route prototype method exists');
-  t.equal(typeof app.toString, 'function', 'app.toString prototype method exists');
-  t.equal(typeof app.start, 'function', 'app.start prototype method exists');
-  t.equal(typeof app.mount, 'function', 'app.mount prototype method exists');
-  t.equal(typeof app.emitter, 'object', 'app.emitter prototype method exists');
+  t.assert.equal(typeof app.route, 'function', 'app.route prototype method exists');
+  t.assert.equal(typeof app.toString, 'function', 'app.toString prototype method exists');
+  t.assert.equal(typeof app.start, 'function', 'app.start prototype method exists');
+  t.assert.equal(typeof app.mount, 'function', 'app.mount prototype method exists');
+  t.assert.equal(typeof app.emitter, 'object', 'app.emitter prototype method exists');
 
-  t.equal(typeof app.emit, 'function', 'app.emit instance method exists');
-  t.equal(typeof app.router, 'object', 'app.router instance object exists');
-  t.equal(typeof app.state, 'object', 'app.state instance object exists');
-
-  t.end();
+  t.assert.equal(typeof app.emit, 'function', 'app.emit instance method exists');
+  t.assert.equal(typeof app.router, 'object', 'app.router instance object exists');
+  t.assert.equal(typeof app.state, 'object', 'app.state instance object exists');
 });
 
-tape('should enable history and href by defaut', t => {
+test('should enable history and href by defaut', t => {
   const app = choo();
-  t.true(app._historyEnabled, 'history enabled');
-  t.true(app._hrefEnabled, 'href enabled');
-  t.end();
+  t.assert.ok(app._historyEnabled, 'history enabled');
+  t.assert.ok(app._hrefEnabled, 'href enabled');
 });
 
-tape('router should pass state and emit to view', t => {
+test('router should pass state and emit to view', (t, done) => {
   t.plan(2);
   const app = choo();
   const container = init();
   app.route('/', (state, emit) => {
-    t.equal(typeof state, 'object', 'state is an object');
-    t.equal(typeof emit, 'function', 'emit is a function');
+    t.assert.equal(typeof state, 'object', 'state is an object');
+    t.assert.equal(typeof emit, 'function', 'emit is a function');
+    done();
     return document.createElement('div');
   });
   app.mount(container);
 });
 
-tape('router should support a default route', t => {
+test('router should support a default route', (t, done) => {
   t.plan(1);
   const app = choo();
   const container = init('/random');
   app.route('*', () => {
-    t.pass();
+    t.assert.ok(true);
+    done();
     return document.createElement('div');
   });
   app.mount(container);
 });
 
-tape('enabling hash routing should treat hashes as slashes', t => {
+test('enabling hash routing should treat hashes as slashes', (t, done) => {
   t.plan(1);
   const app = choo({ hash: true });
   const container = init('/account#security');
   app.route('/account/security', () => {
-    t.pass();
+    t.assert.ok(true);
+    done();
     return document.createElement('div');
   });
   app.mount(container);
 });
 
-tape('router should ignore hashes by default', t => {
+test('router should ignore hashes by default', (t, done) => {
   t.plan(1);
   const app = choo();
   const container = init('/account#security');
   app.route('/account', () => {
-    t.pass();
+    t.assert.ok(true);
+    done();
     return document.createElement('div');
   });
   app.mount(container);
@@ -97,36 +99,38 @@ tape('router should ignore hashes by default', t => {
 
 // built-in state
 
-tape('state should include events', t => {
+test('state should include events', (t, done) => {
   t.plan(2);
   const app = choo();
   const container = init();
   app.route('/', state => {
-    t.ok(Object.hasOwn(state, 'events'), 'state has event property');
-    t.ok(Object.keys(state.events).length > 0, 'events object has keys');
+    t.assert.ok(Object.hasOwn(state, 'events'), 'state has event property');
+    t.assert.ok(Object.keys(state.events).length > 0, 'events object has keys');
+    done();
     return document.createElement('div');
   });
   app.mount(container);
 });
 
-tape('state should include location on render', t => {
+test('state should include location on render', (t, done) => {
   t.plan(6);
   const app = choo();
   const container = init('/foo/bar/file.txt?bin=baz');
   app.route('/:first/:second/*', state => {
     const params = { first: 'foo', second: 'bar', wildcard: 'file.txt' };
-    t.equal(state.href, '/foo/bar/file.txt', 'state has href');
-    t.equal(state.route, ':first/:second/*', 'state has route');
-    t.ok(Object.hasOwn(state, 'params'), 'state has params');
-    t.deepEqual(state.params, params, 'params match');
-    t.ok(Object.hasOwn(state, 'query'), 'state has query');
-    t.deepEqual(state.query, { bin: 'baz' }, 'query match');
+    t.assert.equal(state.href, '/foo/bar/file.txt', 'state has href');
+    t.assert.equal(state.route, ':first/:second/*', 'state has route');
+    t.assert.ok(Object.hasOwn(state, 'params'), 'state has params');
+    t.assert.deepEqual(state.params, params, 'params match');
+    t.assert.ok(Object.hasOwn(state, 'query'), 'state has query');
+    t.assert.deepEqual(state.query, { bin: 'baz' }, 'query match');
+    done();
     return document.createElement('div');
   });
   app.mount(container);
 });
 
-tape('state should include location on store init', t => {
+test('state should include location on store init', (t, done) => {
   t.plan(6);
   const app = choo();
   const container = init('/foo/bar/file.txt?bin=baz');
@@ -136,25 +140,27 @@ tape('state should include location on store init', t => {
 
   function store(state, _emit) {
     const params = { first: 'foo', second: 'bar', wildcard: 'file.txt' };
-    t.equal(state.href, '/foo/bar/file.txt', 'state has href');
-    t.equal(state.route, ':first/:second/*', 'state has route');
-    t.ok(Object.hasOwn(state, 'params'), 'state has params');
-    t.deepEqual(state.params, params, 'params match');
-    t.ok(Object.hasOwn(state, 'query'), 'state has query');
-    t.deepEqual(state.query, { bin: 'baz' }, 'query match');
+    t.assert.equal(state.href, '/foo/bar/file.txt', 'state has href');
+    t.assert.equal(state.route, ':first/:second/*', 'state has route');
+    t.assert.ok(Object.hasOwn(state, 'params'), 'state has params');
+    t.assert.deepEqual(state.params, params, 'params match');
+    t.assert.ok(Object.hasOwn(state, 'query'), 'state has query');
+    t.assert.deepEqual(state.query, { bin: 'baz' }, 'query match');
+    done();
   }
 });
 
-tape('state should include title', t => {
+test('state should include title', (t, done) => {
   t.plan(3);
   document.title = 'foo';
   const app = choo();
   const container = init();
-  t.equal(app.state.title, 'foo', 'title is match');
+  t.assert.equal(app.state.title, 'foo', 'title is match');
   app.use((state, emitter) => {
     emitter.on(state.events.DOMTITLECHANGE, () => {
-      t.equal(state.title, 'bar', 'title is changed in state');
-      t.equal(document.title, 'bar', 'title is changed in document');
+      t.assert.equal(state.title, 'bar', 'title is changed in state');
+      t.assert.equal(document.title, 'bar', 'title is changed in document');
+      done();
     });
   });
   app.route('/', (state, emit) => {
--- test/node.js
+++ test/node.js
@@ -1,107 +1,98 @@
-const tape = require('tape');
+const test = require('node:test');
 
 const choo = require('..');
 
-tape('should expose a public API', t => {
+test('should expose a public API', t => {
   const app = choo();
 
-  t.equal(typeof app.route, 'function', 'app.route prototype method exists');
-  t.equal(typeof app.toString, 'function', 'app.toString prototype method exists');
-  t.equal(typeof app.start, 'function', 'app.start prototype method exists');
-  t.equal(typeof app.mount, 'function', 'app.mount prototype method exists');
-  t.equal(typeof app.emitter, 'object', 'app.emitter prototype method exists');
+  t.assert.equal(typeof app.route, 'function', 'app.route prototype method exists');
+  t.assert.equal(typeof app.toString, 'function', 'app.toString prototype method exists');
+  t.assert.equal(typeof app.start, 'function', 'app.start prototype method exists');
+  t.assert.equal(typeof app.mount, 'function', 'app.mount prototype method exists');
+  t.assert.equal(typeof app.emitter, 'object', 'app.emitter prototype method exists');
 
-  t.equal(typeof app.emit, 'function', 'app.emit instance method exists');
-  t.equal(typeof app.router, 'object', 'app.router instance object exists');
-  t.equal(typeof app.state, 'object', 'app.state instance object exists');
-
-  t.end();
+  t.assert.equal(typeof app.emit, 'function', 'app.emit instance method exists');
+  t.assert.equal(typeof app.router, 'object', 'app.router instance object exists');
+  t.assert.equal(typeof app.state, 'object', 'app.state instance object exists');
 });
 
-tape('should enable history and hash by defaut', t => {
+test('should enable history and hash by defaut', t => {
   const app = choo();
-  t.true(app._historyEnabled, 'history enabled');
-  t.true(app._hrefEnabled, 'href enabled');
-  t.end();
+  t.assert.ok(app._historyEnabled, 'history enabled');
+  t.assert.ok(app._hrefEnabled, 'href enabled');
 });
 
-tape('router should pass state and emit to view', t => {
+test('router should pass state and emit to view', t => {
   t.plan(2);
   const app = choo();
   app.route('/', (state, emit) => {
-    t.equal(typeof state, 'object', 'state is an object');
-    t.equal(typeof emit, 'function', 'emit is a function');
+    t.assert.equal(typeof state, 'object', 'state is an object');
+    t.assert.equal(typeof emit, 'function', 'emit is a function');
     return '<div></div>';
   });
   app.toString('/');
-  t.end();
 });
 
-tape('router should support a default route', t => {
+test('router should support a default route', t => {
   t.plan(1);
   const app = choo();
   app.route('*', (_state, _emit) => {
-    t.pass();
+    t.assert.ok(true);
     return '<div></div>';
   });
   app.toString('/random');
-  t.end();
 });
 
-tape('enabling hash routing should treat hashes as slashes', t => {
+test('enabling hash routing should treat hashes as slashes', t => {
   t.plan(1);
   const app = choo({ hash: true });
   app.route('/account/security', (_state, _emit) => {
-    t.pass();
+    t.assert.ok(true);
     return '<div></div>';
   });
   app.toString('/account#security');
-  t.end();
 });
 
-tape('router should ignore hashes by default', t => {
+test('router should ignore hashes by default', t => {
   t.plan(1);
   const app = choo();
   app.route('/account', (_state, _emit) => {
-    t.pass();
+    t.assert.ok(true);
     return '<div></div>';
   });
   app.toString('/account#security');
-  t.end();
 });
 
 // built-in state
 
-tape('state should include events', t => {
+test('state should include events', t => {
   t.plan(2);
   const app = choo();
   app.route('/', (state, _emit) => {
-    t.ok(Object.hasOwn(state, 'events'), 'state has event property');
-    t.ok(Object.keys(state.events).length > 0, 'events object has keys');
+    t.assert.ok(Object.hasOwn(state, 'events'), 'state has event property');
+    t.assert.ok(Object.keys(state.events).length > 0, 'events object has keys');
     return '<div></div>';
   });
   app.toString('/');
-  t.end();
 });
 
-tape('state should include location on render', t => {
+test('state should include location on render', t => {
   t.plan(6);
   const app = choo();
   app.route('/:first/:second/*', (state, _emit) => {
     const params = { first: 'foo', second: 'bar', wildcard: 'file.txt' };
-    t.equal(state.href, '/foo/bar/file.txt', 'state has href');
-    t.equal(state.route, ':first/:second/*', 'state has route');
-    t.ok(Object.hasOwn(state, 'params'), 'state has params');
-    t.deepEqual(state.params, params, 'params match');
-    t.ok(Object.hasOwn(state, 'query'), 'state has query');
-    t.deepLooseEqual(state.query, { bin: 'baz' }, 'query match');
+    t.assert.equal(state.href, '/foo/bar/file.txt', 'state has href');
+    t.assert.equal(state.route, ':first/:second/*', 'state has route');
+    t.assert.ok(Object.hasOwn(state, 'params'), 'state has params');
+    t.assert.deepEqual(state.params, params, 'params match');
+    t.assert.ok(Object.hasOwn(state, 'query'), 'state has query');
+    t.assert.deepEqual(state.query, { bin: 'baz' }, 'query match');
     return '<div></div>';
   });
   app.toString('/foo/bar/file.txt?bin=baz');
-  t.end();
 });
 
-tape('state should include location on store init', t => {
+test('state should include location on store init', t => {
   t.plan(6);
   const app = choo();
   app.use(store);
@@ -110,16 +101,16 @@ tape('state should include location on store init', t => {
 
   function store(state, _emit) {
     const params = { first: 'foo', second: 'bar', wildcard: 'file.txt' };
-    t.equal(state.href, '/foo/bar/file.txt', 'state has href');
-    t.equal(state.route, ':first/:second/*', 'state has route');
-    t.ok(Object.hasOwn(state, 'params'), 'state has params');
-    t.deepEqual(state.params, params, 'params match');
-    t.ok(Object.hasOwn(state, 'query'), 'state has query');
-    t.deepLooseEqual(state.query, { bin: 'baz' }, 'query match');
+    t.assert.equal(state.href, '/foo/bar/file.txt', 'state has href');
+    t.assert.equal(state.route, ':first/:second/*', 'state has route');
+    t.assert.ok(Object.hasOwn(state, 'params'), 'state has params');
+    t.assert.deepEqual(state.params, params, 'params match');
+    t.assert.ok(Object.hasOwn(state, 'query'), 'state has query');
+    t.assert.deepEqual(state.query, { bin: 'baz' }, 'query match');
   }
 });
 
-tape('state should not mutate on toString', t => {
+test('state should not mutate on toString', t => {
   t.plan(6);
 
   const app = choo();
@@ -134,14 +125,14 @@ tape('state should not mutate on toString', t => {
   });
 
   for (let i = 0, len = routes.length; i < len; i++) {
-    t.equal(states[i].test, routes[i], 'store was used');
-    t.equal(states[i].title, routes[i], 'title was added to state');
+    t.assert.equal(states[i].test, routes[i], 'store was used');
+    t.assert.equal(states[i].title, routes[i], 'title was added to state');
   }
 
   function store(state, emitter) {
     state.test = null;
     emitter.on('test', str => {
-      t.equal(state.test, null, 'state has been reset');
+      t.assert.equal(state.test, null, 'state has been reset');
       state.test = str;
     });
   }
